fb9f08686368c07637474224238eba032fcb2326
Dirceu Henrique <dhnasci@hotmail.com>
2020-07-06 15:10:38 -0400

    Versão inicial

---
+++ src/main/java/br/manaus/dnconsultoria/agenda/AgendaCultoApplication.java
@@ -0,0 +1,13 @@
+package br.manaus.dnconsultoria.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaCultoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaCultoApplication.class, args);
+	}
+
+}
